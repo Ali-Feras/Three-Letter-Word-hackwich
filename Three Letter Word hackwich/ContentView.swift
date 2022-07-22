2c6bc9aaec0690209e92b3df727ce7d3fd2839c0
Ali Feras <ferasali876@gmail.com>
2022-07-22 09:52:48 -0500

    Initial Commit

---
+++ Three Letter Word hackwich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Three Letter Word hackwich
+//
+//  Created by Ali on 22/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Three Letter Word hackwich/Three_Letter_Word_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_Word_hackwichApp.swift
+//  Three Letter Word hackwich
+//
+//  Created by Ali on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_Word_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
